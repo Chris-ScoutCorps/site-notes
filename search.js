21116bbf85bcd069aeac829c14f22920a75996f8
Chris Shaffer <chris@scoutcorpsllc.com>
2023-05-22 15:31:40 -0400

    sort results

--- search.js
+++ search.js
@@ -1,8 +1,8 @@
 'use strict';
 
 async function clean() {
-  let stored = await STORAGE.get();
-  for (let site of Object.keys(stored)) {
+  const stored = await STORAGE.get();
+  for (const site of Object.keys(stored)) {
     if (site.startsWith('sorts|') && !stored[site].length) {
       STORAGE.remove(site);
     }
@@ -12,43 +12,68 @@ async function clean() {
   }
 }
 
+const MAX_RESULTS = 10;
+
+async function getSearchResults(search) {
+  if (!search) {
+    return;
+  }
+  //alert(e.target.value)
+  //await clean();
+
+  let more = false;
+  let results = [];
+  let stored = await STORAGE.get();
+
+  for (let site of Object.keys(stored)) {
+    if (!site.startsWith('sorts|')) {
+      if (site.toLowerCase().includes(search)) {
+        for (let note of Object.values(stored[site])) {
+          results.push({
+            site,
+            note,
+            score: 3 + (site.toLowerCase().startsWith(search) ? 1 : 0) + (note.startsWith(search) ? 2 : note.includes(search) ? 1 : 0),
+          });
+        }
+      } else {
+        for (let i in stored['sorts|' + site]) {
+          let uuid = stored['sorts|' + site][i];
+          let note = stored[site][uuid];
+          if (note.includes(search)) {
+            let order = 1 - (i / Object.keys(stored[site]).length);
+            results.push({
+              site,
+              note,
+              score: (note.startsWith(search) ? 2 : note.includes(search) ? 1 : 0) + order,
+            });
+          }
+        }
+      }
+
+      if (results.length > MAX_RESULTS && search.length >= 3) {
+        more = true;
+        break;
+      }
+    }
+  }
+
+
+  if (results.length > MAX_RESULTS) {
+    more = true;
+    results = results.slice(0, MAX_RESULTS);
+  }
+
+  results.sort((a, b) => b.score - a.score);
+
+  return results;
+}
+
 document.getElementById('search-txt').value = '';
 
 document.getElementById('search-txt').addEventListener('keyup', (e) => {
   debounce("search", async () => {
-    let search = e.target.value.toLowerCase();
-    if (!search) {
-      return;
-    }
-    //alert(e.target.value)
-    //await clean();
-
-    let results = [];
-    let stored = await STORAGE.get();
-    for (let site of Object.keys(stored)) {
-      if (!site.startsWith('sorts|')) {
-        if (site.toLowerCase().includes(search)) {
-          for (let note of Object.values(stored[site])) {
-            results.push({
-              site,
-              note,
-              score: 3 + (site.toLowerCase().startsWith(search) ? 1 : 0) + (note.startsWith(search) ? 2 : note.includes(search) ? 1 : 0),
-            });
-          }
-        } else {
-          for (let note of Object.values(stored[site])) {
-            if (note.includes(search)) {
-              results.push({
-                site,
-                note,
-                score: note.startsWith(search) ? 2 : note.includes(search) ? 1 : 0,
-              });
-            }
-          }
-        }
-      }
-    }
-    
+    const search = e.target.value.toLowerCase();
+    const results = await getSearchResults(search);
     //alert(JSON.stringify(results));
   });
 });
