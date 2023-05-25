4595d7f20d05528847507f3210fa513123eec5a0
Chris Shaffer <chris@scoutcorpsllc.com>
2023-05-25 18:27:08 -0400

    consolidate setup scripts

--- setup.js
+++ setup.js
@@ -1,11 +1,60 @@
-//'use strict';
+'use strict';
 
-function debounce(key, callback, timeout = 250) {
-  clearTimeout(SiteNotes.DEBOUNCES[key]);
-  SiteNotes.DEBOUNCES[key] = setTimeout(callback, timeout);
+const IS_CHROME = (() => {
+  const isChromium = window.chrome;
+  const winNav = window.navigator;
+  const vendorName = winNav.vendor;
+  const isOpera = typeof window.opr !== "undefined";
+  const isIEedge = winNav.userAgent.indexOf("Edg") > -1;
+  const isIOSChrome = winNav.userAgent.match("CriOS");
+
+  return isIOSChrome || (
+    isChromium !== null &&
+    typeof isChromium !== "undefined" &&
+    vendorName === "Google Inc." &&
+    isOpera === false &&
+    isIEedge === false);
+})();
+
+const SiteNotes = IS_CHROME ? {
+  SIDEBAR_OPEN: 'site-notes-sidebar-open',
+  SIDEBAR_TOGGLE_ID: 'sidebar-toggle-button',
+  STORAGE: chrome.storage.local,
+  IS_CHROME: true,
+} : {
+  IS_CHROME: false,
+  STORAGE: browser.storage.local,
+  TABS: browser.tabs,
+  WINDOWS: browser.windows,
+  WINDOW_ID: undefined,
 }
 
-function populateBody() {
+if (!SiteNotes.IS_CHROME) {
+  SiteNotes.WINDOWS.getCurrent({ populate: true }).then((windowInfo) => {
+    SiteNotes.WINDOW_ID = windowInfo.id;
+  });
+}
+
+SiteNotes.DEBOUNCES = {};
+SiteNotes.debounce = (key, callback, timeout = 250) => {
+  clearTimeout(SiteNotes.DEBOUNCES[key]);
+  SiteNotes.DEBOUNCES[key] = setTimeout(callback, timeout);
+};
+
+(async function populateBody() {
+  if (SiteNotes.IS_CHROME) {
+    const frame = document.createElement('div');
+    frame.id = 'site-notes-body';
+    frame.className = 'sidebar';
+    frame.style.width = '250px';
+    frame.style.top = '0px';
+    frame.style.right = '0px';
+    frame.style.position = 'fixed';
+    frame.style.display = 'none';
+    frame.style.zIndex = 9999999;
+    document.body.appendChild(frame);
+  }
+
   if (document.getElementById('site-notes-body')) {
     document.getElementById('site-notes-body').innerHTML = `
     <input type="button" value="sidebar" id="sidebar-toggle-button" style="display: none;" />
@@ -22,25 +71,29 @@ function populateBody() {
     <div id="page-notes" style="width: 100%;"></div>
   `;
   }
-}
 
-if (typeof SiteNotes === 'undefined') {
-  SiteNotes = {
-    IS_CHROME: false,
-    STORAGE: browser.storage.local,
-    TABS: browser.tabs,
-    WINDOWS: browser.windows,
-    WINDOW_ID: undefined,
-  };
-}
+  if (SiteNotes.IS_CHROME) {
+    const isOpen = async () => ((await SiteNotes.STORAGE.get(SiteNotes.SIDEBAR_OPEN)) || {})[SiteNotes.SIDEBAR_OPEN] === true;
 
-if (!SiteNotes.IS_CHROME) {
-  SiteNotes.WINDOWS.getCurrent({ populate: true }).then((windowInfo) => {
-    SiteNotes.WINDOW_ID = windowInfo.id;
-  });
-}
+    const button = document.getElementById(SiteNotes.SIDEBAR_TOGGLE_ID);
+    button.style.display = 'block';
+    button.value = `sidebar ${(await isOpen()) ? ' >' : ' <'}`
 
-SiteNotes.DEBOUNCES = {};
-SiteNotes.debounce = debounce;
+    button.addEventListener("click", async () => {
+      await SiteNotes.STORAGE.set({
+        [SiteNotes.SIDEBAR_OPEN]: !(await isOpen()),
+      });
+      button.value = `sidebar ${(await isOpen()) ? ' >' : ' <'}`
+    });
 
-populateBody();
+    async function showSidebar() {
+      const open = await isOpen();
+      document.getElementById('site-notes-body').style.display = open ? 'block' : 'none';
+    }
+
+    showSidebar();
+    setInterval(async () => {
+      chrome.runtime?.id && showSidebar();
+    }, 250);
+  }
+})();
