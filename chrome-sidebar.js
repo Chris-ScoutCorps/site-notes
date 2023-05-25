368140959fbabe644905f30e0685723fe411e7f4
Chris Shaffer <chris@scoutcorpsllc.com>
2023-05-25 13:45:03 -0400

    making some more progress with chrome ...

--- chrome-sidebar.js
+++ chrome-sidebar.js
@@ -3,33 +3,18 @@
 const SIDEBAR_URL = chrome.runtime.getURL('sidebar/sidebar.html');
 const SIDEBAR_OPEN = 'site-notes-sidebar-open';
 
-const isOpen = async () => ((await STORAGE.get(SIDEBAR_OPEN)) || {})[SIDEBAR_OPEN] === true;
-
-//function appendScript(src, el) {
-//  let script = document.createElement('script');
-
-//  script.setAttribute('src', src);
-//  script.setAttribute('type', 'text/javascript');
-//  script.setAttribute('async', 'true');
-
-//  el.appendChild(script);
-//}
-
-async function populateSidebar() {
-  const open = await isOpen();
-  if (open && !document.getElementById(SIDEBAR_URL)) {
-    const frame = document.createElement('div');
-    frame.id = SIDEBAR_URL;
-    frame.style.width = '250px';
-    frame.style.top = '0px';
-    frame.style.right = '0px';
-    frame.style.height = '100vw';
-    frame.style.position = 'fixed';
-    frame.style.borderLeft = '4px solid gray';
-    frame.style.zIndex = 9999999;
-    frame.style.backgroundColor = 'white';
-
-    frame.innerHTML = `
+const frame = document.createElement('div');
+frame.id = SIDEBAR_URL;
+frame.style.width = '250px';
+frame.style.top = '0px';
+frame.style.right = '0px';
+frame.style.height = '100vw';
+frame.style.position = 'fixed';
+frame.style.display = 'none';
+frame.style.borderLeft = '4px solid gray';
+frame.style.zIndex = 9999999;
+frame.style.backgroundColor = 'white';
+frame.innerHTML = `
     <input type="button" value="sidebar" id="sidebar-toggle-button" style="display: none;" />
 
     <div id="search-area" style="width: 100%; margin-top: 0;">
@@ -43,14 +28,16 @@ async function populateSidebar() {
     <h4 id="page-notes-lbl" style="width: 100%;"></h4>
     <div id="page-notes" style="width: 100%;"></div>
   `;
+document.body.appendChild(frame);
 
-    document.body.appendChild(frame);
-    //appendScript(chrome.runtime.getURL('popup/popup.js'), frame);
-    //appendScript(chrome.runtime.getURL('notes.js'), frame);
-    //appendScript(chrome.runtime.getURL('search.js'), frame);
-  } else if (!open && document.getElementById(SIDEBAR_URL)) {
-    document.getElementById(SIDEBAR_URL).remove();
-  }
+const isOpen = async () => ((await STORAGE.get(SIDEBAR_OPEN)) || {})[SIDEBAR_OPEN] === true;
+
+async function showSidebar() {
+  const open = await isOpen();
+  document.getElementById(SIDEBAR_URL).style.display = open ? 'block' : 'none';
 }
-populateSidebar();
-let interval = setInterval(async () => { chrome.runtime?.id && populateSidebar(); }, 250);
+
+showSidebar();
+let interval = setInterval(async () => {
+  chrome.runtime?.id && showSidebar();
+}, 250);
